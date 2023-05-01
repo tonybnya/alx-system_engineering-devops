8ef10fcb6d3389e49a4cb8b1bd2e9e67274d838f
Tony <nya.tony2010@gmail.com>
2023-05-01 19:12:23 +0100

    Update: 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,15 +12,16 @@ if __name__ == "__main__":
     id_ = sys.argv[1]
     users_url = URL + 'users/' + id_
     todos_url = URL + 'todos'
-    
+
     user = requests.get(users_url).json()
     todos = requests.get(todos_url, params={"userId": id_}).json()
-    
-    tasksdone = [t.get('title') for t in todos if t.get('completed') is True]
-    
-    done = len(tasksdone)
+
+    dones = [i.get('title') for i in todos if i.get('completed') is True]
+
+    done = len(dones)
     name = user.get('name')
     tasks = len(todos)
-    
+
     print("Employee {} is done with tasks({}:{})".format(name, done, tasks))
-    [print("\t {}".format(task)) for task in tasks_done]
+    for task in dones:
+        print("\t {}".format(task))
