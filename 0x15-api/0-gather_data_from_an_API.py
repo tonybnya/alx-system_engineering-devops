e7edd623af9c9f5324bbb05b6c848f7d6c6c353d
tonybnya <nya.tony2010@gmail.com>
2023-05-01 10:19:37 -0700

    Update: 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,26 +9,26 @@ URL = 'https://jsonplaceholder.typicode.com/'
 
 
 def main():
-    '''Main function.'''
-    ID = sys.argv[1]
-    # users = requests.get(f"{URL}users/{ID}").json()
-    # todos = requests.get(f"{URL}todos/", params={'userId': ID}).json()
-    users = requests.get("{}users/{}".format(URL, ID)).json()
-    todos = requests.get("{}todos/".format(URL), params={'userId': ID}).json()
-    tasks = [
-        task.get('title') for task in todos if task.get('completed') is True
-    ]
-    done = len(tasks)
-    num = len(todos)
+	'''Main function.'''
+	ID = sys.argv[1]
+	# users = requests.get(f"{URL}users/{ID}").json()
+	# todos = requests.get(f"{URL}todos/", params={'userId': ID}).json()
+	users = requests.get("{}users/{}".format(URL, ID)).json()
+	todos = requests.get("{}todos/".format(URL), params={'userId': ID}).json()
+	tasks = [
+		task.get('title') for task in todos if task.get('completed') is True
+	]
+	done = len(tasks)
+	num = len(todos)
 
-    # print(f"Employee {users.get('name')} is done withs tasks({done}:{num})")
-    print("Employee {} is done withs tasks({}:{})".format(
-        users.get('name'), done, num)
-    )
+	# print(f"Employee {users.get('name')} is done withs tasks({done}:{num})")
+	print("Employee {} is done withs tasks({}:{})".format(
+		users.get('name'), done, num)
+	)
 
-    for task in tasks:
-        print(f"\t{task}")
+	for task in tasks:
+		print(f"\t{task}")
 
 
 if __name__ == '__main__':
-    main()
+	main()
