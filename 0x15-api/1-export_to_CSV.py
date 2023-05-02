d6fb119854d5df23b17f2de44d9be2cbdcea44d9
Tony <nya.tony2010@gmail.com>
2023-05-02 09:24:02 +0100

    Update: 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     URL = 'https://jsonplaceholder.typicode.com/'
     id_ = sys.argv[1]
     users_url = URL + 'users/' + id_
-    todos_url = URL + 'todos'
+    tasks_url = URL + 'todos'
 
     user = requests.get(users_url).json()
-    username = user.get('username')
-    todos = requests.get(todos_url, params={"userId": id_}).json()
+    name = user.get('username')
+    tasks = requests.get(tasks_url, params={"userId": id_}).json()
 
-    with open("{}.csv".format(id_), "w", newline="") as file_obj:
-        writer = csv.writer(file_obj, quoting=csv.QUOTE_ALL)
-
-        for todo in todos:
-            writer.writerow([id_, todo.get("completed"), todo.get("title")])
+    with open("{}.csv".format(id_), "w") as obj:
+        for task in tasks:
+            status = task.get("completed")
+            title = task.get("title")
+            obj.write('"{}","{}","{}","{}"'.format(id_, name, status, title))
