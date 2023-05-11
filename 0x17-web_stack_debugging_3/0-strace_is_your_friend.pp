b2e450b77a9ccb82105eca1c830814a2dfe6d50c
Tony <nya.tony2010@gmail.com>
2023-05-11 12:17:16 +0100

    Update: 0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,4 @@
-file { '/var/www/html/wp-content/cache':
-    ensure => 'absent',
-    recurse => true,
-}
-
-service { 'apache2':
-    ensure => 'running',
-    enable => true,
-    require => File['/var/www/html/wp-content/cache'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
