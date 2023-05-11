3ec09fbc32599186b24b1d3de32f43ca00cf0d70
Tony <nya.tony2010@gmail.com>
2023-05-11 12:19:51 +0100

    Update: 0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Changes `phpp` extensions to the correct ones: `php`
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
