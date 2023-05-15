b906398682f45cf2d3085ea035716ef15c3d9eff
Tony <nya.tony2010@gmail.com>
2023-05-15 12:23:53 +0100

    Add: 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+# 0-subs.py
+# Author: @tonybnya
+
+Python script queries the Reddit API and returns the number of subscribers.
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    Main function
+    """
+    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'tonybnya'}
+    res = requests.get(url, headers=headers)
+    subscribers = res.json().get('data', {}).get('subscribers', 0)
+
+    return subscribers
