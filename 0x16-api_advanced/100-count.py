9234a5aa079c634e4aa8989c28598606cb0c22bc
Tony <nya.tony2010@gmail.com>
2023-05-15 14:00:51 +0100

    Update: 10-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,18 +9,18 @@ title of all hot articles, prints a sorted count of given keywords.
 import requests
 
 
-def count_words(subreddit, word_list, after='', words_count={}):
+def count_words(subreddit, word_list, after='', words={}):
     """
     Main function
     """
-    if not words_count:
+    if not words:
         for word in word_list:
-            if word.lower() not in words_count:
-                words_count[word.lower()] = 0
+            if word.lower() not in words:
+                words[word.lower()] = 0
 
     if after is None:
-        words_count = sorted(words_count.items(), key=lambda x: (-x[1], x[0]))
-        for word in words_count:
+        lst = sorted(words.items(), key=lambda x: (-x[1], x[0]))
+        for word in lst:
             if word[1]:
                 print('{}: {}'.format(word[0], word[1]))
         return None
@@ -43,11 +43,11 @@ def count_words(subreddit, word_list, after='', words_count={}):
         after = res.json()['data']['after']
         for child in children:
             title = child['data']['title']
-            lower = [word.lower() for word in title.split(' ')]
+            low = [word.lower() for word in title.split(' ')]
 
-            for word in words_count.keys():
-                words_count[word] += lower.count(word)
+            for word in words.keys():
+                words[word] += low.count(word)
     except Exception:
         return None
 
-    count_words(subreddit, word_list, after, words_count)
+    count_words(subreddit, word_list, after, words)
