b45e60a8c8b95af9f506fa5da4ed898c1cf9c29b
Tony <nya.tony2010@gmail.com>
2023-05-15 13:53:40 +0100

    Update: 10-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,17 +9,25 @@ title of all hot articles, prints a sorted count of given keywords.
 import requests
 
 
-def count_words(subreddit, word_list, after='', count=0, words_count={}):
+def count_words(subreddit, word_list, after='', words_count={}):
     """
     Main function
     """
-    url = 'https://reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': 'tonybnya'}
-    params = {
-        'after': after,
-        'count': count,
-        'limit': 100
-    }
+    if not words_count:
+        for word in word_list:
+            if word.lower() not in words_count:
+                words_count[word.lower()] = 0
+
+    if after is None:
+        words_count = sorted(words_count.items(), key=lambda x: (-x[1], x[0]))
+        for word in words_count:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'user-agent': 'tonybnya'}
+    params = {'limit': 100, 'after': after}
     res = requests.get(
         url,
         headers=headers,
@@ -27,33 +35,19 @@ def count_words(subreddit, word_list, after='', count=0, words_count={}):
         allow_redirects=False
     )
 
+    if res.status_code != 200:
+        return None
+
     try:
-        data = res.json()
-        if res.status_code == 404:
-            raise Exception
+        children = res.json()['data']['children']
+        after = res.json()['data']['after']
+        for child in children:
+            title = child['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in words_count.keys():
+                words_count[word] += lower.count(word)
     except Exception:
-        print('')
-        return
+        return None
 
-    data = res.get('data')
-    after = data.get('after')
-    count += data.get('dist')
-
-    for child in data.get('children'):
-        title = child.get('data').get('title').lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                ntimes = len([t for t in title if t == word.lower()])
-                if words_count.get(word) is None:
-                    words_count[word] = ntimes
-                else:
-                    words_count[word] += ntimes
-
-    if after is None:
-        if len(words_count) == 0:
-            print('')
-            return
-        words_count = sorted(words_count.items(), key=lambda i: (-i[1], i[0]))
-        [print('{}: {}'.format(key, value)) for key, value in words_count]
-    else:
-        count_words(subreddit, word_list, after, count, words_count)
+    count_words(subreddit, word_list, after, words_count)
