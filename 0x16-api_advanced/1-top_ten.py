6db5fad6f1ed8dd3dc5aa30f33b4fd586629e5ac
Tony <nya.tony2010@gmail.com>
2023-08-10 18:13:06 +0100

    Add: 1-main.py & 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+# 1-top_ten.py
+# Author: @tonybnya
+
+Python script queries the Reddit API and prints the titles of the first 10
+host posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Main function
+    """
+    url = 'https://reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'tonybnya'}
+    res = requests.get(url, headers=headers)
+
+    try:
+        children = res.json().get('data').get('children')
+        for child in children:
+            print(child.get('data').get('title'))
+    except Exception:
+        print(None)
