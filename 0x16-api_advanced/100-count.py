c527070559e74fc4aefddce4ba99f94d4c1677b3
Tony <nya.tony2010@gmail.com>
2023-08-10 18:24:51 +0100

    Add: 100-main.py & 100-count.py

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+# 100-count.py
+# Author: @tonybnya
+
+Python script has a recursive function that queries Reddit API, parses the
+title of all hot articles, prints a sorted count of given keywords.
+"""
+import requests
+
+
+def count_words(subreddit, word_list, after='', words={}):
+    """
+    Main function
+    """
+    if not words:
+        for word in word_list:
+            if word.lower() not in words:
+                words[word.lower()] = 0
+
+    if after is None:
+        lst = sorted(words.items(), key=lambda x: (-x[1], x[0]))
+        for word in lst:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'user-agent': 'tonybnya'}
+    params = {'limit': 100, 'after': after}
+    res = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
+    if res.status_code != 200:
+        return None
+
+    try:
+        children = res.json()['data']['children']
+        after = res.json()['data']['after']
+        for child in children:
+            title = child['data']['title']
+            low = [word.lower() for word in title.split(' ')]
+
+            for word in words.keys():
+                words[word] += low.count(word)
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, after, words)
