6c2501385868b44b11f96d8f7eeeac5080987f99
Tony <nya.tony2010@gmail.com>
2023-08-17 20:34:48 +0100

    Add: '0x1A-application_server' directory

--- 1-user_limit.pp
+++
@@ -1,6 +0,0 @@
-# Change the OS configuration
-
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
-}
